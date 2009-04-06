a041a871ad2a51f6ce8e8a6263b842b87c1f92b9
ryan@thinkrefresh.com <ryan@thinkrefresh.com>
2009-04-06 12:23:59 +0100

    Removed the requirement for start date or issue number, because not all switch/solos require them

--- lib/active_merchant/billing/credit_card.rb
+++ lib/active_merchant/billing/credit_card.rb
@@ -142,9 +142,9 @@ module ActiveMerchant #:nodoc:
       def validate_switch_or_solo_attributes #:nodoc:
         if %w[switch solo].include?(type)
           unless valid_month?(@start_month) && valid_start_year?(@start_year) || valid_issue_number?(@issue_number)
-            errors.add :start_month,  "is invalid"      unless valid_month?(@start_month)
-            errors.add :start_year,   "is invalid"      unless valid_start_year?(@start_year)
-            errors.add :issue_number, "cannot be empty" unless valid_issue_number?(@issue_number)
+            errors.add :start_month,  "is invalid"      unless @start_month == 0 || valid_month?(@start_month)
+            errors.add :start_year,   "is invalid"      unless @start_year == 0 || valid_start_year?(@start_year)
+            errors.add :issue_number, "cannot be empty" unless @issue_number.blank? || valid_issue_number?(@issue_number)
           end
         end
       end
